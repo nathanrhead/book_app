184a5cf0bb30d345f33c631b5e853cf2e6a442c7
Nathan Rhead Cox <nrc4@hotmail.com>
2020-11-18 16:17:59 -0800

    Completed feature 2 of lab 12 for CF 301.

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ dotenv.config();
 const PORT = process.env.PORT || 3333;
 const client = new pg.Client(process.env.DATABASE_URL);
 
-
-
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,38 +19,35 @@ app.set('view engine', 'ejs');
 app.get('/', renderHomePage);
 app.get('/searches/new', showForm);
 app.post('/add-book', saveBook);
-// app.get('/view-details/:listItems_id', bookDetails); //TODO: <-----THIS
-
+app.get('/view-details/:listItems_id', bookDetails);
 
 app.post('/searches', createSearch);
 
+function bookDetails(req, res) {
+  let SQL = 'SELECT * FROM seed WHERE id=$1';
+  let values = [req.params.listItems_id];
+
+  return client.query(SQL, values)
+    .then(data => {
+      res.render('pages/books/show', { oneBook: data.rows[0] })
+    });
+}
+
 function saveBook(req, res) {
-  // console.log('hello')
-  //TODO: check if it's in DB
+  let { title, authors, description, image, isbn, category } = req.body;
 
-  //if true - select from DB and respond by rending? to index.ejs
-  //if false - INSERT to the DB (SQL) SELECT FROM DB and render to index.ejs
-  let { title, authors, description, image } = req.body;
-  
-  // const title = req.body.title;
-  // const authors = req.body.authors;
-  // const description = req.body.description;
-  // const image = req.body.image;
-
-  let SQL = 'INSERT INTO seed (title, authors, description, image) VALUES ($1, $2, $3, $4) RETURNING *;';
-  let values = [title, authors, description, image];
+  let SQL = 'INSERT INTO seed (title, authors, description, image, isbn, category) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;';
+  let values = [title, authors, description, image, isbn, category];
 
   return client.query(SQL, values)
     .then(res.redirect('/'))
     .catch(err => console.error(err));
 }
 
-
-
 function renderHomePage(req, res) {
   let SQL = 'SELECT * FROM seed;';
 
-  return client.query(SQL)
+  client.query(SQL)
     .then(data => {
       res.render('pages/index', {bananas: data.rows})
     })
@@ -70,24 +65,22 @@ function createSearch(req, res) {
   if(req.body.search[1] === 'title') { url += `+intitle:${req.body.search[0]}`; }
   if(req.body.search[1] === 'author') { url += `+inauthor:${req.body.search[0]}`; }
 
-
   superagent.get(url)
     .then(data => {
-      // console.log(data.body.items);
+      // console.log(data.body.info);
       return data.body.items.map(book => {
+        // console.log(book.volumeInfo);
         return new Book(book.volumeInfo);
       });
     })
     .then(results => {
-      // console.log(results);
+      console.log(results);
       res.render('pages/searches/show', { searchResults: results })
     })
     .catch(err => console.log(err));
 }
 
 function Book(info) {
-  // console.log(info[0]);
-  // console.log(info.imageLinks.smallThumbnail.match(/^http:\/\//g));
   if(info.imageLinks) {
     if(info.imageLinks.smallThumbnail.match(/^http:\/\//g)){
       info.imageLinks.smallThumbnail = info.imageLinks.smallThumbnail.replace('http://', 'https://');
@@ -99,7 +92,16 @@ function Book(info) {
   this.title = info.title || 'No title is available.';
   this.authors = info.authors || 'No author is available.';
   this.description = info.description || 'No description is available.';
-  // this.image = info.imageLinks.smallThumbnail || 'https://i.imgur.com/J5LVHEL.jpg';
+  if (info.category) {
+    this.category = info.category;
+  } else {
+    this.category = 'No category is available.';
+  }
+  if (info.industryIdentifiers) {
+    this.isbn = info.industryIdentifiers[0].identifier;
+  } else {
+    this.isbn = 'No ISBN is available.';
+  }
 }
 
 app.get('*', (req, res) => {
